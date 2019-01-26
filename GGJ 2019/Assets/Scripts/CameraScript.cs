1b60c74d59112b39d477fc360727829a9e880be8
FahmyHady <fahmy.hady@hotmail.com>
2019-01-26 20:40:47 +0200

    no message

---
+++ GGJ 2019/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+  public  Transform player;
+    Vector3 offset;
+    private void Start()
+    {
+        offset = new Vector3(-5, 0, 10);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
