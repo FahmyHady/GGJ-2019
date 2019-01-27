c4c7b56f964c7568b7fa1286884002257cfbc369
ahmedsamir05 <ahmedsamir04@gmail.com>
2019-01-27 15:34:02 +0200

    randomSound

---
+++ GGJ 2019/Assets/Scripts/SoundEffector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    AudioClip [] myEffects;
+    //AudioSource.PlayClipAtPoint(miaw1, new Vector2(0, 0));
+    void Start()
+    {
+        StartCoroutine(StartEffect());
+    }
+    IEnumerator StartEffect()
+    {
+        yield return new WaitForSeconds(2);
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(4,8.0f));
+            AudioSource.PlayClipAtPoint(myEffects[Random.Range(0, myEffects.Length)], GameManager.instance.mainCamera.transform.position);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
