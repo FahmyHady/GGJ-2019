95057ec357bbe285fc30f215c7bdbde3761483e3
ahmedsamir05 <ahmedsamir04@gmail.com>
2019-01-27 15:46:07 +0200

    restart

--- GGJ 2019/Assets/Scripts/GameManager.cs
+++ GGJ 2019/Assets/Scripts/GameManager.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public static GameManager instance;
     [HideInInspector] public Camera mainCamera;
-    void Start()
+    void Awake()
     {
         instance = this;
         mainCamera = Camera.main;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
